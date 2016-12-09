f6a00b9d908552cd11a9c979d8e5109e92811130
tcloaken <trevor.enright@gmail.com>
2016-12-09 15:44:14 -0800

    added create_db

---
+++ create_db.py
@@ -0,0 +1,32 @@
+"""
+Create the database for the specified user
+(who must not be siteUserAdmin)
+"""
+
+import pymongo
+from pymongo import MongoClient
+import sys
+
+import secrets.admin_secrets
+import secrets.client_secrets
+
+MONGO_ADMIN_URL = "mongodb://{}:{}@{}:{}/admin".format(
+    secrets.admin_secrets.admin_user,
+    secrets.admin_secrets.admin_pw,
+    secrets.admin_secrets.host, 
+    secrets.admin_secrets.port)
+
+try: 
+    dbclient = MongoClient(MONGO_ADMIN_URL)
+    db = getattr(dbclient, secrets.client_secrets.db)
+    print("Got database {}".format(secrets.client_secrets.db))
+    print("Attempting to create user")
+    db.createUser(secrets.client_secrets.db_user,
+                password=secrets.client_secrets.db_user_pw)
+    print("Created user {}".format(secrets.client_secrets.db_user))
+    collection = db.dated
+except Exception as err:
+    print("Failed")
+    print(err)
+
+
